3d9902ea9d2eeaa890d1bf32dc7960fde14fbadc
Alexander Polyakov <aleksandr19>
2016-11-16 19:08:43 +0300

    edit

--- app/js/jquery.countdown.js
+++ app/js/jquery.countdown.js
@@ -23,49 +23,68 @@ CountdownTimer.prototype = {
         if(timeBetween > 0){
             var ZeroDay = parseInt(this.addZero(day).slice(-1));
             if (ZeroDay >= 2 && ZeroDay <=4) {
-                var day_name = 'Дня';
+                var day_name = 'дня';
             } else if ((ZeroDay == 0) || (ZeroDay >= 5 && ZeroDay <=9)) {
-                var day_name = 'Дней';
+                var day_name = 'дней';
             } else if (ZeroDay == 1) {
-                var day_name = 'День';
+                var day_name = 'день';
             }
 
             var ZeroHour = parseInt(this.addZero(hour).slice(-1));
             if ((ZeroHour == 1) && (ZeroHour == 21)) {
-                var hour_name = 'Час';
+                var hour_name = 'час';
             } else if ((ZeroHour >= 2 && ZeroHour <= 4) || (ZeroHour >= 22 && ZeroHour <= 24)) {
-                var hour_name = 'Часа';
+                var hour_name = 'часа';
             } else if ((ZeroHour == 0) || (ZeroHour >= 5 && ZeroHour <= 20)) {
-                var hour_name = 'Часов';
+                var hour_name = 'часов';
             }
 
 
             var ZeroMin = parseInt(this.addZero(min).slice(-1));
-            console.log(ZeroMin);
             if (ZeroMin == 1) {
-                var min_name = 'Минута';
+                if (parseInt(this.addZero(min).slice(-2) == 11 )) {
+                    var min_name = 'минут';
+                } else {
+                    var min_name = 'минута';
+                }
             } else if (ZeroMin >= 3 && ZeroMin <= 4) {
-                var min_name = 'Минуты';
+                var min_name = 'минуты';
             } else if (ZeroMin == 2) {
                 if (parseInt(this.addZero(min).slice(-2) == 12 )) {
-                    var min_name = 'Минут';
+                    var min_name = 'минут';
                 } else {
-                    var min_name = 'Минуты';
+                    var min_name = 'минуты';
                 }
             } else if ((ZeroMin == 0) ||(ZeroMin >= 5 && ZeroMin <= 9) ) {
-                var min_name = 'Минут';
+                var min_name = 'минут';
             }
 
-
-
+            var ZeroSec = parseInt(this.addZero(sec).slice(-1));
+            if (ZeroSec == 1) {
+                if (parseInt(this.addZero(sec).slice(-2) == 11 )) {
+                    var sec_name = 'секунд';
+                } else {
+                    var sec_name = 'секунда';
+                }
+            } else if (ZeroSec >= 3 && ZeroSec <= 4) {
+                var sec_name = 'секунды';
+            } else if (ZeroSec == 2) {
+                if (parseInt(this.addZero(sec).slice(-2) == 12 )) {
+                    var sec_name = 'секунд';
+                } else {
+                    var sec_name = 'секунды';
+                }
+            } else if ((ZeroSec == 0) ||(ZeroSec >= 5 && ZeroSec <= 9) ) {
+                var sec_name = 'секунд';
+            }
 
 
             timer += '<div class="countDays"><span class="position"><span class="digit">' + this.addZero(day)  + '</span><span class="digit_name">' +day_name+ '</span></span></div>';
             timer += '<div class="countHours"><span class="position"><span class="digit">' + this.addZero(hour)  + '</span><span class="digit_name">' +hour_name+ '</span></span></div>';
-            timer += '<div class="countMinutes"><span class="position"><span class="digit">' + this.addZero(min) + '</span><span class="min_name">' +min_name+ '</span></span></div>';
-            timer += '<div class="countSeconds"><span class="position"><span class="digit">' + this.addZero(sec) + '</span></span></div>';
+            timer += '<div class="countMinutes"><span class="position"><span class="digit">' + this.addZero(min) + '</span><span class="digit_name">' +min_name+ '</span></span></div>';
+            timer += '<div class="countSeconds"><span class="position"><span class="digit">' + this.addZero(sec) + '</span><span class="digit_name">' +sec_name+ '</span></span></div>';
             this.elem.innerHTML = timer;
-            tid = setTimeout((function (_this) { return function () { _this.countDown(); }})(this), 10);
+            tid = setTimeout((function (_this) { return function () { _this.countDown(); }})(this), 100);
         }
         else{
             this.elem.innerHTML = '<span class="number-wrapper"><div class="line"></div><span class="number end"></span></span>';
